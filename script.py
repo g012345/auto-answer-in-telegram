31ae4cd575b2d949cc405a754ec0de2c12af0d47
Egor <egorgrot89136@gmail.com>
2023-11-14 20:37:35 +0300

    option 2

--- script.py
+++ script.py
@@ -1,44 +1,35 @@
-from pyrogram import Client, filters
+from pyrogram import Client,filters
 import time
-import sqlite3
 
-api_id = "---------"
-api_hash = "-----------"
 
-app = Client("account", api_hash=api_hash, api_id=api_id)
+api_id =  ""
+api_hash = "" 
 
-def create_connection():
-    conn = sqlite3.connect('user_data.db')
-    cursor = conn.cursor()
-    return conn, cursor
 
-prepared_message = """Hello, i am egor. please wait me for my free time,i call you at the moment""" 
+app = Client("account",api_hash=api_hash,api_id=api_id)
+
+
 
 @app.on_message(filters.command("otvet", prefixes=".") & filters.me)
 def type(_, msg):
-    msg.delete()
-5
-@app.on_message(filters.private)
-def avtootvet(client, message):
-    user_id = message.from_user.id
-
-    conn, cursor = create_connection()
-
-    try:
-        cursor.execute('SELECT * FROM users WHERE user_id = ?', (user_id,))
-        user_exists = cursor.fetchone()
-
-        if not user_exists:
+    sp_p=[]
+    orig_text = msg.text.split(".otvet ", maxsplit=1)[1]
+    text = orig_text
+    msg.delete()  
+    @app.on_message(filters.private)
+    def avtootvet(client,message):
+        if message.from_user.id not in sp_p:
             if message.chat.type:
                 try:
-                    app.send_message(chat_id=user_id, text=prepared_message)
+                    app.send_message(chat_id=message.from_user.id,text=text)
                 except:
                     pass
                 time.sleep(0.25)
-                cursor.execute('INSERT INTO users (user_id) VALUES (?)', (user_id,))
-                conn.commit()
+                sp_p.append(message.from_user.id)
+        else:
+            pass
 
-    finally:
-        conn.close()
+
+
 
 app.run()
